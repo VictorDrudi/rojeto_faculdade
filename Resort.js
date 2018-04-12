223aa4e0235f55275464a0d2426db091f4f2b9fa
Victor Drudi <vd28carvalho@gmail.com>
2018-04-11 23:22:43 -0300

    Alterando funções

---
+++ Resort.js
@@ -0,0 +1,18 @@
+<script>
+
+$('.carousel').carousel({
+  interval: 2000
+})
+
+
+
+$(document).ready(function(){
+    //FANCYBOX
+    //https://github.com/fancyapps/fancyBox
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+});
+   
+</script>
